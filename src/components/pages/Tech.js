c2747021df06984365b877211120d1341246970a
AndersonAPinto <andersonaugustopinto@gmail.com>
2024-01-07 11:38:27 -0300

    update Tech

--- src/components/pages/Tech.js
+++ src/components/pages/Tech.js
@@ -13,19 +13,19 @@ const PageTech = () => {
             </div>
             <div className="iconstech">
                 <div className="aligntextbtns">
-                    <button><i class="fa-brands fa-html5" style={{ fontSize: '100px', color: '#003049' }}></i></button>
+                    <div className="tess"><i class="fa-brands fa-html5" style={{ fontSize: '100px', color: '#003049' }}></i></div>
                     <div className="textbtn">&nbsp;HTML, usado para criar e estruturar páginas na web, definindo o conteúdo e a estrutura de documentos web.</div>
-                    <button><i class="fa-brands fa-css3-alt" style={{ fontSize: '100px', color: '#003049' }}></i></button>
+                    <div className="tess"><i class="fa-brands fa-css3-alt" style={{ fontSize: '100px', color: '#003049' }}></i></div>
                     <div className="textbtn">&nbsp;CSS, usado para descrever a apresentação visual, incluindo layout, cores e fontes.</div>
 
                 </div>
             </div>
             <div className="iconstech_">
                 <div  className="aligntextbtns">
-                    <button><i class="fa-brands fa-square-js" style={{ fontSize: '100px', color: '#003049' }}></i></button>
+                    <div className="tess"><i class="fa-brands fa-square-js" style={{ fontSize: '100px', color: '#003049' }}></i></div>
                     <div className="textbtn">&nbsp;JavaScript, linguagem de programação versátil e poderosa usada para adicionar lógicas e funcionalidades.</div>
 
-                    <button><i class="fa-brands fa-react" style={{ fontSize: '100px', color: '#003049' }}></i></button>
+                    <div className="tess"><i class="fa-brands fa-react" style={{ fontSize: '100px', color: '#003049' }}></i></div>
                     <div className="textbtn">&nbsp;React, biblioteca JavaScript usado para construir interfaces de usuário, como Single Page Applications (SPA).</div>
 
                 </div>
